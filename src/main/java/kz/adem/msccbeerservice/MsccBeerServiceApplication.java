8eb8cae7de5139a07da25a9d1abf2e156b3ec2af
Adem <ademshanghai@gmail.com>
2023-06-08 16:45:15 +0600

    initial commit

---
+++ src/main/java/kz/adem/msccbeerservice/MsccBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package kz.adem.msccbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsccBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsccBeerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/adem/msccbeerservice/MsccBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.adem.msccbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsccBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
